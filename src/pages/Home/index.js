6ce4c5484a2eabe3e01234084008a67e41c22a12
Eduardo <cadutapinheiro@gmail.com>
2022-11-11 02:10:13 -0300

    Busca na api e useEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,32 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
+//URL DA API: movie/now_playing?api_key=9568c474fe45ef2c5626c8dce8a08cbc&language=pt-BR
 
 const Home = () => {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+
+      async function loadFilmes() {
+        const response = await api.get("movie/now_playing", {
+          params: {
+            api_key: "9568c474fe45ef2c5626c8dce8a08cbc",
+            language: "pt-BR",
+            page: 1
+          }
+        })
+
+       // console.log(response.data.results.slice(0, 10));
+       
+      }
+
+      loadFilmes();
+
+  }, []);
+  
+
   return (
     <h1>Home</h1>
   )
